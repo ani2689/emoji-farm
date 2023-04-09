52c0ebfac4cef3222258416e32d8f0b090faf27d
ani2689 <ojno7777@gmail.com>
2023-04-09 23:48:31 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/ani/emojifarm/EmojiFarmApplication.java
@@ -0,0 +1,13 @@
+package com.ani.emojifarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmojiFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ani/emojifarm/EmojiFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.emojifarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmojiFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
